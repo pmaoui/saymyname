001a47edd5069a7debe3ca96ce082e4a42a4a7ed
Pierrot <maouipierre+bitbucket@gmail.com>
2016-01-06 22:36:19 +0100

    unit test

---
+++ test/unit.js
@@ -0,0 +1,34 @@
+var SayMyName = require('../saymyname');
+
+
+QUnit.test("Names", function( assert ) {
+  var names = [
+    {
+      "fullname" : "jean de la fontaine",
+      "shouldBe" : "Jean de La Fontaine"
+    },
+    {
+      "firstname" : "Jean baptise",
+      "name"      : "poquelin",
+      "shouldBe"    : "Jean-Baptise Poquelin"
+    },
+    {
+      "fullname" : "pedro suárez-llanos",
+      "shouldBe" : "Pedro Suárez-Llanos"
+    }
+  ];
+
+
+  for (var i=0; i < names.length; i++) {
+    name = names[i];
+    if (typeof name.fullname != "undefined") {
+      var smn = new SayMyName(name.fullname);
+    }
+    else {
+      var smn = new SayMyName(name.firstname,name.name);
+    }
+    var humanized = smn.humanize(); 
+    assert.equal( humanized, name.shouldBe, "We expect "+ humanized+" to be " + name.shouldBe );
+  }
+
+});
